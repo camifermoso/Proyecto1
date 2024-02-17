61de6ef7da78123396980c19a6fa2ef93e343659
camifermoso <cfermoso@correo.unimet.edu.ve>
2024-02-17 15:04:44 -0400

    probando git

--- Proyect1/src/proyect1/Proyect1.java
+++ Proyect1/src/proyect1/Proyect1.java
@@ -16,6 +16,7 @@ public class Proyect1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World"); 
+        System.out.println("Proyecto 1");
     }
     
 }
